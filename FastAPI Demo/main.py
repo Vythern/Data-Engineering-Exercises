d5256d5bbe8c1e024981047237171495a6b5c641
Vythern <58826813+Vythern@users.noreply.github.com>
2026-07-30 10:57:39 -0400

    Update main.py
    Kafka producer with confluent kafka

--- FastAPI Demo/main.py
+++ FastAPI Demo/main.py
@@ -3,11 +3,27 @@ from fastapi.responses import HTMLResponse
 from fastapi import Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from confluent_kafka import Producer
+from datetime import datetime, timezone
+import json
 
-#FastAPI is 
+# User on the web hits a button, triggers javascript, it makes a request, ends up in uvicorn, uvicorn transmits that down to fast api, and we use fast api to 
+# actually make our code here.  This is our app, fast api starts.  
 app = FastAPI()
 
 
+# We will need kafka producers for this assignment.  Default port on local machine for now.  
+config = {
+    "bootstrap.servers": "localhost:9092"
+}
+
+producer = Producer(config) # This producer can publish messages.  
+# Messages are basically events.  Eg when a user clicks a button.  
+# We will 
+
+
+
+# We are going to append some javascript button functioinality to our page.  
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse) # When FASTAPI receives a get request from uvicorn / the user's request on the browser at the /home endpoint, run this function:  
@@ -61,16 +77,43 @@ def dir_to_home():
 # "Each button should send a post request to a fastAPI backend that logs the event message to the console."  
 
 
+
+def publish_event(event_type: str):
+
+    event = {
+        "event_type": event_type,
+        "timestamp": datetime.now(timezone.utc).isoformat()
+    }
+
+    print(event)
+
+    producer.produce(
+        topic="events",
+        value=json.dumps(event)
+    )
+
+    # For debugging purposes, we are temporarily making kafka synchronous.  
+    producer.flush()
+
+
 # When the user clicks the order or event buttons, we have embdedded a java script script to handle, fetch, and make a post to /order and /event.  
 # Then these functions get called
 @app.post("/order")
 def log_order():
+
+    #Create a kafka message
+    publish_event("order")
+
     print("Order event received")
     return {"message": "Order logged"}
 
 
 @app.post("/navigation")
 def log_navigation():
+
+    #Create a kafka message
+    publish_event("navigation")
+
     print("Navigation event received")
     return {"message": "Navigation logged"}
 
